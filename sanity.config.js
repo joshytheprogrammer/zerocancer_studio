016b1d7b5e21c71de3618fef3f8adef157384777
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-03-23 21:01:50 +0100

    made changes: ''

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,12 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {presentationTool} from 'sanity/presentation'
+
+const SANITY_STUDIO_PREVIEW_URL = (
+	process.env.SANITY_STUDIO_PREVIEW_URL // Dont forget to set this in netlify
+	|| 'http://localhost:3000'
+)
 
 export default defineConfig({
   name: 'default',
@@ -10,9 +16,19 @@ export default defineConfig({
   projectId: 'ftcg31zr',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool(), 
+    visionTool(),
+    presentationTool({
+      previewUrl: SANITY_STUDIO_PREVIEW_URL,
+      title: 'Zerocancer Presentation Tool',
+      name: 'zerocancer'
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
   },
+
+  
 })
